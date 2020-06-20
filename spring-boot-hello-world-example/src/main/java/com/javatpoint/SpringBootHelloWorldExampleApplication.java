92a6069b4fc70b14fcd0bd68dd785ac40b444413
unknown <umadongre92@gmail.com>
2020-06-20 16:14:56 +0530

    sumit

--- spring-boot-hello-world-example/src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
+++ spring-boot-hello-world-example/src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootHelloWorldExampleApplication
 {
-public static void main(String[] args) 
-{
-SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);    
-}
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);    
+	}
 }
