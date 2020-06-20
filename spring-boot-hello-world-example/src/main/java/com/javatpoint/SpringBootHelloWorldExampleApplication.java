e24e8278b6b7c50955fc0eb9158c578f5fd949d6
unknown <umadongre92@gmail.com>
2020-06-20 15:46:17 +0530

    uma commited

--- spring-boot-hello-world-example/src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
+++ spring-boot-hello-world-example/src/main/java/com/javatpoint/SpringBootHelloWorldExampleApplication.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-package com.javatpoint;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringBootHelloWorldExampleApplication
-{
-public static void main(String[] args) 
-{
-    SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);
-}
-=======
 package com.javatpoint;
 
 import org.springframework.boot.SpringApplication;
@@ -24,5 +10,5 @@ public static void main(String[] args)
 {
 SpringApplication.run(SpringBootHelloWorldExampleApplication.class, args);    
 }
->>>>>>> bb37e76b364278ed5c4239fe566047877092d331
+
 }
